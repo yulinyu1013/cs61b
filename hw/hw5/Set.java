e36874ce527550c9197eb5b64b22bf485026d525
Yulin Yu <60793200+yulinyu1120@users.noreply.github.com>
2021-06-17 14:57:27 +0800

    add more test cases and update intersect()

--- hw/hw5/Set.java
+++ hw/hw5/Set.java
@@ -124,37 +124,55 @@ public class Set {
    **/
   
   public void intersect(Set s) {
-	  ListNode s1 = set.front(); 
-	  ListNode s2 = s.set.front();
-	  
-	  try {
-		  while(s1.isValidNode()) {
-			  while (s2.isValidNode()) {
-				  if(!s1.item().equals(s2.item())) { 
-					  s2 = s2.next();
+	 if(set.isEmpty() || s.set.isEmpty()) {//if one of the two set is empty
+		  set = new DList();
+	  }else {
+		  ListNode s1 = set.front(); 
+		  ListNode s2 = s.set.front();
+		  
+		  try {
+			  while(s1.isValidNode()) {
+				  while (s2.isValidNode()) {
+					  if(((Comparable)s1.item()).compareTo(s2.item())==0) {
+						  s1 = s1.next();
+						  s2 = s2.next();
+//						  System.out.println("s1 now: " + s1.item());
+//						  System.out.println("s2 now: "+ s2.item());
+//						  System.out.println("---");
+						    
+					  }else if(((Comparable)s1.item()).compareTo(s2.item())>0) {
+						  s2 = s2.next();
+						  if(!s2.isValidNode()) {
+							  s1.remove();
+						  }
+//						  System.out.println("s1 now: " + s1.item());
+//						  System.out.println("s2 now: "+ s2.item());
+//						  System.out.println("---");
+					  }
+					  else { //<0
+						  s1 = s1.next();
+						  s1.prev().remove();
+//						  System.out.println("s1 now: " + s1.item());
+//						  System.out.println("s2 now: "+ s2.item());
+//						  System.out.println("---");
+					  } 
+					  
 				  }
-				  else {
-					  break;
-				  }  
-			  }
+				  if(s1.isValidNode()&&!s2.isValidNode()) {
+					  s1.remove();
+				  }
+
+			  }		  
 			  
-			  if(!s2.isValidNode()) { //meaning find the current node not in s
-				//remove the current node
-				  s1.remove(); 
-				//reset s2
-				  s2 = s.set.front();	   
-			  }else {
-				  //move to the next
-				  s1 = s1.next();
-			  }  	
-			  
+		  }catch(InvalidNodeException e) {
+			  System.err.println(e);
 		  }
 		  
-	  }catch(InvalidNodeException e) {
-		  System.err.println(e);
+		  
 	  }
 	 
 	  
+  }
 	  
 	  
 	  
@@ -194,31 +212,70 @@ public class Set {
   }
 
   public static void main(String[] argv) {
-    Set s = new Set();
-    s.insert(new Integer(3));
-    s.insert(new Integer(4));
-    s.insert(new Integer(3));
-    System.out.println("Set s = " + s);
+	  System.out.println("Testing insert()");
+	  Set s = new Set();
+	  s.insert(new Integer(3));
+	  s.insert(new Integer(4));
+	  s.insert(new Integer(3));
+	  System.out.println("Set s should be { 3 4 }: " + s);
 
-    Set s2 = new Set();
-    s2.insert(new Integer(4));
-    s2.insert(new Integer(5));
-    s2.insert(new Integer(5));
-    System.out.println("Set s2 = " + s2);
+	  Set s2 = new Set();
+	  s2.insert(new Integer(4));
+	  s2.insert(new Integer(5));
+	  s2.insert(new Integer(5));
+	  System.out.println("Set s2 should be { 4 5 }: " + s2);
 
-    Set s3 = new Set();
-    s3.insert(new Integer(5));
-    s3.insert(new Integer(3));
-    s3.insert(new Integer(8));
-    System.out.println("Set s3 = " + s3);
+	  Set s3 = new Set();
+	  s3.insert(new Integer(5));
+	  s3.insert(new Integer(3));
+	  s3.insert(new Integer(8));
+	  System.out.println("Set s3 should be { 3 5 8 }: " + s3);
 
-    s.union(s2);
-    System.out.println("After s.union(s2), s = " + s);
+	  System.out.println();
+	  System.out.println("Tesing union()");
+	  s.union(s2);
+	  System.out.println("After s.union(s2), s should be { 3 4 5 }: " + s);
+	  s2.union(s3);
+	  System.out.println("After s2.union(s3), s2 should be { 3 4 5 8 }: " + s2);
+	  Set s4 = new Set();
+	  System.out.println("Empty set s4 = " + s4);
+	  s.union(s4);
+	  System.out.println("After s.union(s4), s should be { 3 4 5 }: " + s);
+	  s4.union(s);
+	  System.out.println("After s4.union(s), s4 should be { 3 4 5 }: " + s4);
 
-    s.intersect(s3);
-    System.out.println("After s.intersect(s3), s = " + s);
+	  System.out.println();
+	  System.out.println("Tesing intersect()");
+	  Set s5 = new Set();
+	  Set s6 = new Set();
+	  s6.insert(new Integer(1));
+	  s5.intersect(s6);
+	  System.out.println("{}.intersect({1}) should be { }: " + s5);
+	  s6.intersect(s5);
+	  System.out.println("{1}.intersect({}) should be { }: " + s6);
+	  s6.insert(new Integer(1));
+	  Set s7 = new Set();
+	  s7.insert(new Integer(1));
+	  s7.insert(new Integer(2));
+	  s6.intersect(s7);
+	  System.out.println("{1}.intersect({1 2}) should be { 1 }: " + s6);
+	  s6.insert(new Integer(2));
+	  s6.insert(new Integer(3));
+	  s6.intersect(s7);
+	  System.out.println("{1 2 3}.intersect({1 2}) should be { 1 2 }: " + s6);
+	  s6.insert(new Integer(3));
+	  s6.insert(new Integer(5));
+	  s7.insert(new Integer(4));
+	  s7.insert(new Integer(7));
+	  s7.intersect(s6);
+	  System.out.println("{1 2 4 7}.intersect({1 2 3 5}) should be { 1 2 }: " + s7);
 
-    System.out.println("s.cardinality() = " + s.cardinality());
-    // You may want to add more (ungraded) test code here.
-  }
+	  System.out.println();
+	  System.out.println("Tesing cardinality()");
+	  System.out.println("s.cardinality() should be 3: " + s.cardinality());
+	  System.out.println("s4.cardinality() should be 3: " + s4.cardinality());
+	  System.out.println("s5.cardinality() should be 0: " + s5.cardinality());
+	  System.out.println("s6.cardinality() should be 4: " + s6.cardinality());
+	  System.out.println("s7.cardinality() should be 2: " + s7.cardinality());
+	}
 }
