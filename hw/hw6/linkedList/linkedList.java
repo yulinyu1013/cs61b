e7a9d40398f3fa627cc6b942e934d6354e82eb64
Yulin Yu <60793200+yulinyu1120@users.noreply.github.com>
2021-06-24 20:26:15 +0800

    Add files via upload

---
+++ hw/hw6/linkedList/linkedList.java
@@ -0,0 +1,20 @@
+package linkedList;
+import dict.*;
+
+
+public class linkedList {
+	
+	public linkedListNode head;
+	public int size;
+	
+	public linkedList() {//empty linkedlist
+		head = null;
+		size = 0;
+	}
+	
+
+	
+	
+	
+
+}
---
+++ hw/hw6/linkedList/linkedListNode.java
@@ -0,0 +1,21 @@
+
+
+package linkedList;
+import dict.*;
+
+public class linkedListNode extends Entry {
+	
+//	public Object key;
+//	public Object value;
+	public linkedListNode next;
+	
+	public linkedListNode(Object key, Object value){
+		this.key = key;
+		this.value = value;
+		
+	}
+	
+	public linkedListNode(){
+
+	}
+}
