86307bc8bca9e4588c2fa4dd920f9d168292fbd9
Yulin Yu <60793200+yulinyu1120@users.noreply.github.com>
2021-07-04 20:33:23 +0800

    Add files via upload

---
+++ lab/lab10/Entry.java
@@ -0,0 +1,27 @@
+/* Entry.java */
+
+//package dict;
+
+/**
+ *  A class for dictionary entries.
+ *
+ *  DO NOT CHANGE THIS FILE.  It is part of the interface of the
+ *  Dictionary ADT.
+ **/
+
+public class Entry {
+
+  protected Object key;
+  protected Object value;
+  
+  //no constructor here: default Entry();
+
+  public Object key() {
+    return key;
+  }
+
+  public Object value() {
+    return value;
+  }
+
+}
