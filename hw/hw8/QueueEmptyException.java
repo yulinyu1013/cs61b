78ac140014485535c4b494e6fd753e3958a0e51b
Yulin Yu <60793200+yulinyu1120@users.noreply.github.com>
2021-07-13 20:52:19 +0800

    Add files via upload

---
+++ hw/hw8/QueueEmptyException.java
@@ -0,0 +1,15 @@
+/* QueueEmptyException.java */
+
+package list3;
+
+public class QueueEmptyException extends Exception {
+
+  public QueueEmptyException() {
+    super();
+  }
+
+  public QueueEmptyException(String s) {
+    super(s);
+  }
+
+}
